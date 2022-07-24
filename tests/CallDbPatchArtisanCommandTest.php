58228f5c82fe0cd74abcea6625a69fa2322612c7
Shisha <shishatw225@gmail.com>
2022-07-24 22:25:14 +0800

    增加 filter

--- tests/CallDbPatchArtisanCommandTest.php
+++ tests/CallDbPatchArtisanCommandTest.php
@@ -2,22 +2,27 @@
 
 namespace Tests;
 
-use Illuminate\Support\Facades\DB;
+use Illuminate\Testing\PendingCommand;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class CallDbPatchArtisanCommandTest extends TestCase
 {
-    use DatabaseMigrations;
+    use TestArtisanCommandHelpers;
 
     protected static $published;
 
     protected function setUp(): void
     {
         parent::setUp();
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ian')->unique();
+            $table->string('name');
+        });
 
         if (empty(static::$published)) {
             Artisan::call('vendor:publish', [
@@ -34,32 +39,19 @@ class CallDbPatchArtisanCommandTest extends TestCase
         @rmdir(static::$published);
     }
 
-    /**
-     * @param  string  $option
-     * @return string
-     */
-    private function parseLabel(string $option): string
+    private function expectsCommandChoice(PendingCommand $command, $answer): PendingCommand
     {
-        return "-> {$option}";
-    }
+        $options = [
+            $this->parseLabel('2022_07_19_000000_add_priority_to_products_table.php'),
+        ];
 
-    /**
-     * @param  string  $table
-     * @param  string  $column
-     * @return self
-     */
-    private function assertDatabaseTableHasColumn($table, $column)
-    {
-        $this->assertTrue(
-            Schema::hasColumn($table, $column),
-            sprintf(
-                'The table [%s] doesn\'t have the column named %s',
-                $table,
-                $column
-            )
+        $command->expectsChoice(
+            '選擇補丁檔案',
+            $this->parseLabel($answer),
+            $options,
         );
 
-        return $this;
+        return $command;
     }
 
     // =========================================================================
@@ -68,20 +60,11 @@ class CallDbPatchArtisanCommandTest extends TestCase
 
     public function test_call_artisan_command()
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('ian')->unique();
-            $table->string('name');
-        });
-
         $command = $this->artisan('db:patch');
 
-        $command->expectsChoice(
-            '選擇補丁檔案',
-            $this->parseLabel('2022_07_19_000000_add_priority_to_products_table.php'),
-            [
-                $this->parseLabel('2022_07_19_000000_add_priority_to_products_table.php'),
-            ],
+        $this->expectsCommandChoice(
+            $command,
+            '2022_07_19_000000_add_priority_to_products_table.php'
         );
 
         $command->expectsOutput(sprintf(
@@ -100,4 +83,41 @@ class CallDbPatchArtisanCommandTest extends TestCase
     }
 
     //  @depends
+
+    public function test_call_artisan_command_with_filter()
+    {
+        $command = $this->artisan('db:patch', [
+            'filter' => 'product',
+        ]);
+
+        $this->expectsCommandChoice(
+            $command,
+            '2022_07_19_000000_add_priority_to_products_table.php'
+        );
+
+        $command->expectsOutput(sprintf(
+            'Running: php artisan migrate --path=%s',
+            $this->resolvePath('/database/patches/2022_07_19_000000_add_priority_to_products_table.php')
+        ));
+
+        $command->run();
+
+        $this->assertDatabaseHas(
+            'migrations',
+            ['migration' => '2022_07_19_000000_add_priority_to_products_table']
+        );
+
+        $this->assertDatabaseTableHasColumn('products', 'priority');
+    }
+
+    public function test_call_artisan_command_with_filter_then_not_found()
+    {
+        $command = $this->artisan('db:patch', [
+            'filter' => '__IMIFUMEI__',
+        ]);
+
+        $command->expectsOutput('找不到符合的補丁檔案');
+        $command->assertExitCode(1);
+        $command->run();
+    }
 }
---
+++ tests/TestArtisanCommandHelpers.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Support\Facades\Schema;
+
+trait TestArtisanCommandHelpers
+{
+    /**
+     * @param  string  $option
+     * @return string
+     */
+    private function parseLabel(string $option): string
+    {
+        return "-> {$option}";
+    }
+
+    /**
+     * @param  string  $table
+     * @param  string  $column
+     * @return self
+     */
+    private function assertDatabaseTableHasColumn($table, $column)
+    {
+        $this->assertTrue(
+            Schema::hasColumn($table, $column),
+            sprintf(
+                'The table [%s] doesn\'t have the column named %s',
+                $table,
+                $column
+            )
+        );
+
+        return $this;
+    }
+}
